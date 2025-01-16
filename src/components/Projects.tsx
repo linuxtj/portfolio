67c777941e08c508e638863209e613ecbee3c7a6
linuxtj <linuxtj19@gmail.com>
2025-01-16 16:02:04 -0700

    Add k8s-ansible-cluster picture

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import Title from "./Title";
 import ProjectsCard from "./ProjectsCard";
-import {  loganalisys, linuxpatching, rsyncbackup,  userprovisioning, centralizedconfigmanagement, terraformcloudsetup } from "../assets";
+import {  loganalisys, linuxpatching, rsyncbackup, k8s-ansible-cluster,  userprovisioning, centralizedconfigmanagement, terraformcloudsetup } from "../assets";
 import { FadeIn } from "./FadeIn";
 
 const Projects = () => {
@@ -25,7 +25,7 @@ const Projects = () => {
           <ProjectsCard
             title="k8s-ansible-cluster"
             des=""
-            src={centralizedconfigmanagement}
+            src={k8s-ansible-cluster}
           />
           <ProjectsCard
             title="Hybrid Cloud Setup with Terraform"
